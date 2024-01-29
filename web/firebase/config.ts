be22bca1bd3d7c9946ec85c925eedb8d206e3686
Bharat Kashyap <bharatkashyap@outlook.com>
2024-01-29 12:05:38 +0530

    fix: Compatible with Vercel

--- web/firebase/config.ts
+++ web/firebase/config.ts
@@ -1,19 +1,27 @@
 import { initializeApp, getApps } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { promises as fs } from "fs";
 
 async function initialize() {
   // Your web app's Firebase configuration
-  if (!process.env.FIREBASE_CREDENTIAL_PATH)
+  if (
+    !process.env.FIREBASE_API_KEY ||
+    !process.env.FIREBASE_PROJECT_ID ||
+    !process.env.FIREBASE_AUTH_DOMAIN ||
+    !process.env.FIREBASE_STORAGE_BUCKET ||
+    !process.env.FIREBASE_MESSAGING_SENDER_ID ||
+    !process.env.FIREBASE_APP_ID
+  )
     throw new Error("Firebase credentials not provided in .env.local");
 
-  const firebaseCredentialPath =
-    process.cwd() + process.env.FIREBASE_CREDENTIAL_PATH;
+  const firebaseCredential = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+  };
 
   try {
-    const firebaseCredential = JSON.parse(
-      await fs.readFile(firebaseCredentialPath, "utf-8")
-    );
     return getApps().length === 0
       ? initializeApp(firebaseCredential)
       : getApps()[0];
